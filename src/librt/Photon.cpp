2aee8b4f91bee851d6bf8f45deeaef80ff43f56c
Nathan Lively <ntlively@ufl.edu>
2017-04-18 13:17:55 -0400

    Photon class updated
    intersection, getter, setter all in.

--- src/librt/Photon.cpp
+++ src/librt/Photon.cpp
@@ -3,11 +3,11 @@
 #include "Photon.h"
 
 
-
 Photon::Photon(RGBR_f initColor,STVector3 initDirection,STVector3 initOrigin)
-    :   rgb         		initColor,
-        direction           initDirection,
-        origin              initOrigin,
+    :   rgb         		(initColor),
+        direction           (initDirection),
+        origin              (initOrigin),
+        lastIntersection    (NULL)
 {
 
 }
@@ -16,4 +16,57 @@ Photon::Photon(RGBR_f initColor,STVector3 initDirection,STVector3 initOrigin)
 Photon::~Photon(void)
 {
 
+}
+
+
+//set the photon color
+void Photon::SetColor(RGBR_f color)
+{
+    rgb = color;
+}
+
+
+// return the photon color
+RGBR_f Photon::GetColor(void)
+{
+    return(rbg);
+}
+
+//set the photon direction
+void Photon::SetDirection(STVector3 direc)
+{
+    direction = direc;
+}
+
+
+// return the photon direction
+STVector3 Photon::GetDirection(void)
+{
+    return(direction);
+}
+
+//set the photon origin
+void Photon::SetOrigin(STVector3 orig)
+{
+    origin = orig;
+}
+
+
+// return the photon direction
+STVector3 Photon::GetOrigin(void)
+{
+    return(origin);
+}
+
+//set the photon's last intersection
+void Photon::SetIntersection(Intersection intersection)
+{
+    lastIntersection = intersection;
+}
+
+
+// return the photon's last direction
+Intersection Photon::GetIntersection(void)
+{
+    return(lastIntersection);
 }
--- src/librt/include/Photon.h
+++ src/librt/include/Photon.h
@@ -6,7 +6,7 @@
 
 #include "STVector3.h"
 #include "RGBR_f.h"
-
+#include "Intersection.h"
 //class Surface;
 
 class Photon {
@@ -15,9 +15,34 @@ public:
 
                     Photon                       (RGBR_f initColor,STVector3 initDirection,STVector3 initOrigin);
                     ~Photon                      (void);
+
+
+void 				SetColor					 (RGBR_f color);
+
+RGBR_f 				GetColor					 (void);
+
+void 				SetDirection				 (STVector3 direc);
+
+STVector3 			GetDirection				 (void);
+
+void 				SetOrigin				 	 (STVector3 orig);
+
+STVector3 			GetOrigin				 	 (void);
+
+void 				SetIntersection				 (Intersection intersection);
+
+Intersection 		GetIntersection				 (void);
+
+
+
+
+
+
+
     RGBR_f 			rgb;
     STVector3       origin;
     STVector3       direction;
+    Intersection    lastIntersection;
 };
 
 
