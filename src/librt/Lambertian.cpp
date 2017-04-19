051717df419c4d18065adb1ea3a061ce8da2d0a1
Jennifer Cremer <jennifercremer@gmail.com>
2017-04-18 22:18:14 -0400

    integrated photons
    into lambert

--- src/librt/Lambertian.cpp
+++ src/librt/Lambertian.cpp
@@ -3,6 +3,7 @@
 #include "RGBR_f.h"
 #include "Surface.h"
 #include <stdio.h>
+#include "PhotonShader.h"
 
 Lambertian::Lambertian (float diffuseValue) : diffuseValue(diffuseValue)
 {
@@ -11,6 +12,8 @@ Lambertian::Lambertian (float diffuseValue) : diffuseValue(diffuseValue)
 
 RGBR_f Lambertian::Shade (Intersection *pIntersection, STVector3 *lightDirection, Light *light, Scene *pScene)
 {
+	RGBR_f photonColor = PhotonShader().Shade(pIntersection, lightDirection, light, pScene);
+
 	double diffuseFactor = std::max(0.0f, STVector3::Dot(pIntersection->normal, *lightDirection));
 	diffuseFactor *= light->GetIntensity(pIntersection->point); // How Intensity effects the light
 	diffuseFactor *= diffuseValue; // How the lambert diffuse coefficient effects the light
@@ -21,5 +24,6 @@ RGBR_f Lambertian::Shade (Intersection *pIntersection, STVector3 *lightDirection
 	// Arbitrarily set the alpha to 255.
 	diffuseColor.a = 255;
 
+	diffuseColor += photonColor;
 	return diffuseColor;
 }
