ff3474aa71e7c8d2650e0a7201cd9bb3ec70514b
Nathan Lively <ntlively@ufl.edu>
2017-04-18 22:07:13 -0400

    Fixed Compile Errors
    fixed errors in photonShader.cpp

--- src/librt/PhotonShader.cpp
+++ src/librt/PhotonShader.cpp
@@ -3,6 +3,7 @@
 #include "RGBR_f.h"
 #include "Surface.h"
 #include <stdio.h>
+#include <cmath>
 
 PhotonShader::PhotonShader (void)
 {
@@ -13,14 +14,14 @@ RGBR_f PhotonShader::Shade (Intersection *pIntersection, STVector3 *lightDirecti
 {
 	RGBR_f photonColor;
 
-    std::vector<Photon*> photons = pScene->GetPhotons();
+    std::vector<Photon*> *photons = pScene->GetPhotons();
 
     STVector3 surfNorm = pIntersection->normal;
 
     for(int i = 0; i < photons->size(); i++){
         if((photons->at(i)->GetIntersection().point - pIntersection->point).LengthSq() <= 0.001){
             float weight = std::max(0.0f, STVector3::Dot(surfNorm, photons->at(i)->GetDirection()));
-            weight *= (1.0f - std::sqrt(photons->at(i)->GetDistSquared())) / 50.0f;
+            weight *= (1.0f - sqrt(photons->at(i)->GetDistSquared())) / 50.0f;
 
             photonColor += (photons->at(i)->GetColor() * weight);
             photonColor = RGBR_f::Min(pIntersection->surface->GetColor(), photonColor);
