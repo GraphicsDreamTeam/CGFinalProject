2a4782409f4661493fb6fa0844bb681e477390d0
Jennifer Cremer <jennifercremer@gmail.com>
2017-04-19 00:10:57 -0400

    more stuff

--- src/librt/PhotonShader.cpp
+++ src/librt/PhotonShader.cpp
@@ -19,9 +19,9 @@ RGBR_f PhotonShader::Shade (Intersection *pIntersection, STVector3 *lightDirecti
     STVector3 surfNorm = pIntersection->normal;
 
     for(int i = 0; i < photons->size(); i++){
-        if(((pIntersection->point - photons->at(i)->GetIntersection().point)).LengthSq() <= 0.7){
+        if(((pIntersection->point - photons->at(i)->GetIntersection().point)).LengthSq() <= 0.07){
             float weight = std::max(0.0f, STVector3::Dot(surfNorm, photons->at(i)->GetDirection()));
-            weight *= (5.0f - sqrt(photons->at(i)->GetDistSquared())) / 5.0f;
+            weight *= (1.0f - sqrt(photons->at(i)->GetDistSquared())) / 5.0f;
 
             photonColor += (photons->at(i)->GetColor() * weight);
             photonColor = RGBR_f::Min(pIntersection->surface->GetColor(), photonColor);
